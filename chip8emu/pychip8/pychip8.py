9a495c803e64e88a5e91e23d46b1d9a53b7935c2
Jack Butler <fattredd@gmail.com>
2018-01-25 21:35:15 -0500

    Starting the opcodes

--- chip8emu/pychip8/pychip8.py
+++ chip8emu/pychip8/pychip8.py
@@ -4,11 +4,12 @@
 
 import pyglet
 
+windowScale = 1
+windowH = 32 * windowScale
+windowW = 64 * windowScale
+
 class screen(object):
-    ploy = pyglet.gl.GL_POLYGON
-    self.windowScale = 1
-    self.windowH = 32 * windowScale
-    self.windowW = 64 * windowScale
+    poly = pyglet.gl.GL_POLYGON
 
     def __init__(self):
         self.window = pyglet.window.Window()
@@ -29,13 +30,18 @@ class Chip8(object):
     def __init__(self):
         self.memory = [0]*4096  # RAM
         self.V = [0]*16         # GP Reg
-        self.I = 0           # Index Reg
-        self.PC = 0             # Program Counter
+        self.I = 0              # Index Reg
+        self.PC = 0x200         # Program Counter
 
-        self.DelayT = 0
-        self.SoundT = 0
+        self.DelayT = 0         # Delay Timer
+        self.SoundT = 0         # Sound Timer
 
-        self.display = [False]* (windoW * windowH)
+        self.Stack = [0]*16     # Program Counter Stack
+        self.SP = 0             # Stack Pointer
+
+        self.display = [False]* (self.windowW * self.windowH)
+        self.updateDisplay = False
+        # load fontset here
 
     def loadProgram(self,file):
         with open(file, 'rb') as game:
@@ -43,4 +49,31 @@ class Chip8(object):
                 self.memory[i] = byte
         print("Loaded %s".format(file))
 
-    def pa
+    def runOp(self,offset):
+        opCode = memory[offset] << 8 | memory[offset+1]
+
+        fo = opCode & 0xF000
+        if fo == 0x0000:
+            if opCode&0xFFF == 0xE0
+                # 0x00E0 Display clear
+                self.display = [False]*(self.windowW*self.windowH)
+                self.updateDisplay = True
+            elif opCode&0xFFF == 0xEE:
+                # 0x00EE Return
+                self.PC = self.Stack[self.SP]
+                if self.SP == 0:
+                    exception("Cannot return from subroutine.")
+                else:
+                    self.Stack[self.SP] = self.Stack[self.SP-1]
+                self.SP -= 1
+            else:
+                # 0x0NNN Call
+                self.Stack[self.SP] = self.PC
+                self.SP += 1
+                self.PC = opCode & 0x0FFF
+        elif fo == 0x1000:
+            # 0x1NNN goto
+            self.PC = opCode & 0xFFF
+        elif fo == 0x2000:
+            # 0x2NNN call subroutine
+            self.
